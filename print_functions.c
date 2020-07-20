893c447a7eea845a604e24253df4e98259153c33
ctj01 <flexdancer507@gamil.com>
2020-07-20 16:35:56 -0500

    add funtcion

--- print_functions.c
+++ print_functions.c
@@ -77,7 +77,7 @@ int (*_iSplaceholder(const char *format, ...))(va_list)
 
 int _check_arg(const char *format, ...)
 {
-	unsigned int i = 0, con = 0;
+	unsigned int i = 0, count = 0;
 	va_list list;
 	int (*flag)(va_list);
 
@@ -89,19 +89,26 @@ int _check_arg(const char *format, ...)
 		for (; format[i] != '%' && format[i]; i++)
 		{
 			_putchar(format[i]);
-			con++;
+			count++;
 		}
 		if (!format[i])
-			return (con);
+			return (count);
 		flag = _iSplaceholder(&format[i + 1]);
 		if (flag != NULL)
 		{
-			con += flag(list);
-			i++;
+			count += flag(list);
+			i += 2;
+			continue;
 		}
-		con++;
-		i++;
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
 	va_end(list);
-	return (con);
+	return (count);
 }
