ab0e2029d20e45c3a71d0d451106d2605816bcef
ctj01 <flexdancer507@gamil.com>
2020-07-20 20:37:37 -0500

    funcion number

---
+++ print_number.c
@@ -0,0 +1,89 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_d - decimal
+ * @d : list arguments passed
+ * Return: Always 1
+ */
+
+int print_d(va_list d)
+{
+	int *a;
+	int j, m, n, sum, count, t;
+
+	a = malloc(sizeof(va_arg(d, double)) * 4);
+	count = 0;
+	m = 1000000000;
+
+	n = va_arg(d, int);
+	t =  n / m;
+	*a = t;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	free(a);
+	return (count);
+}
+
+/**
+ * print_int - print integer
+ * @i : list arguments passed
+ * Return: Always 1
+ */
+
+int print_int(va_list i)
+{
+	int *a;
+	int j, m, n, sum, count, t;
+
+	a = malloc(sizeof(va_arg(i, int)) * 4);
+	count = 0;
+	m = 1000000000;
+
+	n = va_arg(i, int);
+	t =  n / m;
+	*a = t;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	free(a);
+	return (count);
+}
