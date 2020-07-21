b1a1f2d49e74234d9920161e4c027132fa1e00d3
ctj01 <flexdancer507@gamil.com>
2020-07-20 22:52:25 -0500

    funcion fixing

--- print_functions.c
+++ print_functions.c
@@ -47,7 +47,7 @@ int prints_char(va_list s)
  */
 
 
-static int (*_iSplaceholder(const char *format, ...))(va_list)
+int (*_iSplaceholder(const char *format, ...))(va_list)
 {
 	unsigned int  d = 0;
 
@@ -81,7 +81,7 @@ int _check_arg(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
 	va_list list;
-	static int (*flag)(va_list);
+	int (*flag)(va_list);
 
 	if (format == NULL)
 		return (-1);
@@ -114,5 +114,3 @@ int _check_arg(const char *format, ...)
 	va_end(list);
 	return (count);
 }
-
-
