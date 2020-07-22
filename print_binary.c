f00c28710fb59221ca25b6036a3b67031154cdb9
ctj01 <flexdancer507@gamil.com>
2020-07-21 22:55:05 -0500

    add function to convert number to binary

--- print_binary.c
+++ print_binary.c
@@ -11,14 +11,16 @@
 int print_binary(va_list b)
 {
 	int count, i;
-	unsigned int a[100];
+	unsigned int *a;
+
 	unsigned int num;
 
 	num = va_arg(b, unsigned int);
+	a = malloc(sizeof(num));
 
 	for (i = 0; num > 0; i++)
 	{
-		a[i] = num % 2;
+		a[i]= num % 2;
 		num = num / 2;
 	}
 	for (i = i -1; i >= 0; i--)
@@ -26,7 +28,7 @@ int print_binary(va_list b)
 		_putchar(a[i] + '0');
 		count++;
 	}
-	count = count - 1;
+	free(a);
 	return (count);
 
 }
