747bf5d7d6c1013816f36a5ede5ad87effbc3afb
ctj01 <flexdancer507@gamil.com>
2020-07-21 21:26:57 -0500

    add function to convert number to binary

---
+++ print_binary.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - convert numer to binary
+ * @b: num to printed
+ * Return: len of integer
+ */
+
+int print_binary(va_list b)
+{
+	int count = 0;
+	int a[100], i;
+	unsigned int num = va_arg(b, unsigned int);
+
+	if (num > 0)
+	{
+		for (i = 0; num > 0; i++)
+		{
+			a[i] = num % 2;
+			num = num / 2;
+		}
+		for (i = i - 1; i >= 0; i--)
+		{
+			_putchar(a[i] + '0');
+			count++;
+		}
+	}
+	return (count);
+}
